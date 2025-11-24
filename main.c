819cd7e14d598e98cc25f5dc674c9f543fdb0cd3
Lukascech84 <lukas-cech@outlook.cz>
2025-11-24 21:26:45 +0100

    02-Textures

--- main.c
+++ main.c
@@ -33,6 +33,14 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    if (IMG_Init(IMG_INIT_PNG) == 0)
+    {
+        SDL_DestroyWindow(window);
+        printf("Error SDL2_image Initialization");
+        SDL_Quit();
+        return 1;
+    }
+
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
     SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
@@ -44,10 +52,14 @@ int main(int argc, char *argv[])
     players = init_Players(players, num_of_players);
     float speed = 200;
 
-    scene scenes[2] = {{.scene_index = 0, .scene_name = "Menu"},
-                       {.scene_index = 1, .scene_name = "Mapa1"}};
+    int num_of_scenes = 2;
+    scene scenes[2] = {{.scene_index = 0, .scene_name = "Menu", .bg_texture_address = "./assets/maps/00_Menu/menu_test.png"},
+                       {.scene_index = 1, .scene_name = "Mapa1", .bg_texture_address = "./assets/maps/01_Grass/map1_scaled.png"}};
 
-    sceneManager sceneManager = {.current_Scene = scenes[0]};
+    sceneManager sceneManager = {.current_Scene = &scenes[0]};
+
+    // Načítání textur
+    load_textures(scenes, num_of_scenes, renderer);
 
     SDL_Event event;
     int running = 1;
@@ -71,20 +83,22 @@ int main(int argc, char *argv[])
 
         // Kód
         int target_scene = 1;
-        if (sceneManager.current_Scene.scene_index != target_scene)
+        if (sceneManager.current_Scene->scene_index != target_scene)
         {
             load_scene(&sceneManager, target_scene, scenes);
             printf("Loading scene: %s\n", scenes[target_scene].scene_name);
         }
+
+        SDL_RenderCopy(renderer, sceneManager.current_Scene->bg_texture, NULL, NULL);
 
         move_Players(players, num_of_players, speed, deltaTime);
         render_Players(players, num_of_players, renderer);
 
         // Debug
 
-        //  printf("Current scene: %s\n", sceneManager.current_Scene.scene_name);
-        //  printf("%dx%d\n", SDL_GetWindowSurface(window)->w, SDL_GetWindowSurface(window)->h);
-        //  printf("x1: %d, y1: %d, x2: %d, y2: %d\n", players[0].hitbox.x, players[0].hitbox.y, players[1].hitbox.x, players[1].hitbox.y);
+        // printf("Current scene: %s\n", sceneManager.current_Scene.scene_name);
+        // printf("%dx%d\n", SDL_GetWindowSurface(window)->w, SDL_GetWindowSurface(window)->h);
+        // printf("x1: %d, y1: %d, x2: %d, y2: %d\n", players[0].hitbox.x, players[0].hitbox.y, players[1].hitbox.x, players[1].hitbox.y);
 
         last = now;
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
@@ -92,6 +106,8 @@ int main(int argc, char *argv[])
     }
 
     clear_Players(players);
+    destroy_textures(scenes, num_of_scenes);
+    IMG_Quit();
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
--- sceneManager.h
+++ sceneManager.h
@@ -8,16 +8,48 @@ typedef struct
     char scene_name[50];
 
     // Map data
+    char *bg_texture_address;
+    SDL_Texture *bg_texture;
 
 } scene;
 
 typedef struct
 {
-    scene current_Scene;
+    scene *current_Scene;
 
 } sceneManager;
 
 void load_scene(sceneManager *scm, int scene_index, scene scenes[])
 {
-    scm->current_Scene = scenes[scene_index];
+    scm->current_Scene = &scenes[scene_index];
+}
+
+int load_textures(scene scenes[], int num_of_scenes, SDL_Renderer *renderer)
+{
+    for (size_t i = 0; i < num_of_scenes; i++)
+    {
+        SDL_Surface *background_sur = IMG_Load(scenes[i].bg_texture_address);
+        if (background_sur == NULL)
+        {
+            printf("Error loading image: %s", IMG_GetError());
+            return 1;
+        }
+        SDL_Texture *background_tex = SDL_CreateTextureFromSurface(renderer, background_sur);
+        if (background_tex == NULL)
+        {
+            printf("Error creating texture");
+            return 1;
+        }
+        scenes[i].bg_texture = background_tex;
+        SDL_FreeSurface(background_sur);
+    }
+    return 0;
+}
+
+void destroy_textures(scene scenes[], int num_of_scenes)
+{
+    for (size_t i = 0; i < num_of_scenes; i++)
+    {
+        SDL_DestroyTexture(scenes[i].bg_texture);
+    }
 }
